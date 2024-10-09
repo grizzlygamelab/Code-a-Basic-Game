52a3df2c4e48b857c1e9d0cf9f99d2ac19470d9d
Justin Scott <hello@jrs.dev>
2024-10-09 08:09:32 -0700

    Moving the camera

--- Roll-a-Ball-Game/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball-Game/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Speed at which the player moves.
+    public float speed = 0;
+
+    // Movement along X and Y axes.
     private float movementX;
     private float movementY;
+    // Rigidbody of the player.
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
     }
 
-    // FixedUpdate is called once per physics update
+    // FixedUpdate is called once per fixed frame-rate frame.
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
     }
 
+    // This function is called when a move input is detected
     private void OnMove(InputValue movementValue)
     {
+        // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
