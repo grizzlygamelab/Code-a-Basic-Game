8cdc220b907fc931384113a90364d4979dd61955
Justin Scott <hello@jrs.dev>
2024-10-15 13:28:23 -0700

    Upto Ai navigation

--- Roll-a-Ball-Game/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball-Game/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Speed at which the player moves.
     public float speed = 0;
 
+    // Set the count in the UI
+    public TextMeshProUGUI countText;
+
+    // Set the win text in the UI
+    public TextMeshProUGUI winText;
+
     // Movement along X and Y axes.
     private float movementX;
     private float movementY;
+
+    private int count;
+
     // Rigidbody of the player.
     private Rigidbody rb;
 
@@ -17,6 +27,15 @@ public class PlayerController : MonoBehaviour
     {
         // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
+
+        //Initialize the count variable
+        count = 0;
+
+        // Display the current starting count value in the UI
+        SetCountText();
+
+        // Set the win text to to false at start.
+        winText.gameObject.SetActive(false);
     }
 
     // FixedUpdate is called once per fixed frame-rate frame.
@@ -38,5 +57,31 @@ public class PlayerController : MonoBehaviour
         // Store the X and Y components of the movement.
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if ( count >= 8)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+
+            // Add 1 to the count variable
+            count = count + 1;
+
+            // Update the UI with the new count value
+            SetCountText();
+        }
     }
 }
